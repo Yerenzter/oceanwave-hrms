2c1cd2716d4966ad02d44936089ec6f1df25b2c5
Yerenzter <yerenzter@yandex.com>
2023-08-05 07:09:30 +0800

    file: oceanwave-server-side-sources

---
+++ oceanwave.bat
@@ -0,0 +1,6 @@
+@color 1f
+@title Ocean Wave - Management System
+@echo off
+
+@echo System server is now starting...
+@node server.js
--- server.js
+++ server.js
@@ -1,66 +1,73 @@
+const { exec } = require("child_process");
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 1024;
-
-const { exec } = require("child_process");
+const ports = [10000, 10001, 10002, 10003, 10004, 10005, 10006]
 const databases = [
-	"json-server -p 10 public/data/config/config.json",
-	"json-server -p 24 public/data/employee/employee.json",
+    "data/config/config.json",
+    "data/user/user.json",
+    "data/user/profile.json",
+    "data/organization/organization.json",
+    "data/attendance/attendance.json",
+    "data/leave/leave.json",
+    "data/authenticator/authenticator.json"
 ];
 
-for(let r=0; r < databases.length; r++) {
-	exec(databases[r], (err, stdout, stderr) => {
-		if(err) {
-			console.log("Database server is fail to run or successfully terminate its process.");
-			return;
-		}
-		
-		console.log("Running");
-	});	
+for (let r = 0; r < databases.length; r++) {
+    exec(`json-server -p ${ports[r]} ${databases[r]}`, (err, stdout, stderr) => {
+        if(err) {
+            console.log(`Database server "http://localhost:${ports[r]}" is failed to run or successfully terminated its process.`);
+        }
+    });
 }
 
-app.use(express.static("public"));
-
-app.listen(port, () => {
-	console.log(`
-Hi, Admin!
-
-=========================================================================
-                 WELCOME TO OCEANWAVE HR MANAGEMENT SYSTEM
-                                  v0.0.1
-                                  
- The robust, convenient & powerful HRM system that fits in your machine.
-=========================================================================
-
-
-Configuration DB server is now running at "http://localhost:10" and its process...
-Employee management DB server is now running at "http://localhost:24" and its process...
-
-DB RESOURCES:
-http://localhost:10/config
-http://localhost:10/config/1
-
-http://localhost:24/employee
-http://localhost:24/employee/1 and so on...
-
-Main system is now running at "http://localhost:${port}"...
-
-Now open your favorite browser and enter this address "https://localhost:${port}" to you addresss bar.
-Press F11 key to make it fullscreen in your browser to improve experience.
-
-
-
-
-
-Enjoy :).
-`);
-});
+console.log(``);
+console.log("RESOURCE                               STATUS");
+for (let r = 0; r < ports.length; r++) {
+    let response = new Response(`http://localhost:${ports[r]}`);
+    console.log(`http://localhost:${ports[r]}                   ${response.status}`);
+}
 
 exec("start msedge http://localhost:1024", (err, stdout, stderr) => {
-	if(err) {
-		console.log(err);
-		return;	
-	}
-	
-	console.log(stdout);	
+    if(err) {
+        exec("start chrome http://localhost:1024", (err, stdout, stderr) => {
+            if(err) console.log(`
+System is failed to run due to absence of Edge or Chrome broswer.
+So open your favorite browser and enter this server address at "http://localhost:1024".
+`);
+        });
+    }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`
+====================================================================================================
+                                        WELCOME TO OCEAN WAVE
+                                                v0.0.1
+
+        A free and open-source HR management system powered by NodeJS, JSON-server and lowDB.
+====================================================================================================
+
+Name: Ocean Wave - HR System
+Developer: Qubonic Arts & Software, AYCQ. (Yerenzter)
+Address: Mabuyong Forest, Brgy. Magcalon, San Jose de Buenavista, Antique 5700, PH
+Version: 0.0.1
+Build state: In-beta
+Model: Open-source
+Price: Free
+License: AYCQ.
+Language used: HTML5, CSS3 & JavaScript
+Database: AJAX (follows the CRUD standard)
+Implementations: Material Icons, NodeJS, ExpressJS, JSON-server & lowDB
+
+
+System server is now running at "http://localhost:${port}"...
+        `);
 });
